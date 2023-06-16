008bf5eab8d4514d40cb382e1bd88fc4e0aee3be
«Oksana <averinaoy@gmail.com>
2023-06-16 15:04:34 +0500

    script2

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
-let name = promt("??? ??? ??????", "???");
-alert(`??????, ${name}`);
+// let name = prompt("??? ??? ??????", "???");
+// alert(`??????, ${name}`);
+
+// function show_text(name){
+//     alert(`Hello,${name}`);
+// }
+// let name = prompt("What is your name?", "Name");
+// show_text(name);
+
+// if(confirm("Well?"))
+//     alert("Ok");
+// else
+//     alert("This is problem");
+
+// const age = prompt("How old are you?");
+// switch(age) {
+//     case '18':
+//         alert("Hurray, everything is possible!");
+//         break;
+//     case '10':
+//         alert("Go to sleep!");
+//         break;
+//     case '30':
+//         alert("I have to go to work tomorrow!");
+//         break;
+//     default:
+//         alert("We don't know what to do");
+//         break;            
+// }
+
+function show_text(name){
+    alert(`${name}`);
+}
+
+let name = prompt("What is your name?", "Name");
+show_text(name);
---
+++ script_dz.js
@@ -0,0 +1,6 @@
+function show_text(name){
+     alert(`${name}`);
+}
+
+let name = prompt("What is your name?", "Name");
+show_text(name);
